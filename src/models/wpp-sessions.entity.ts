8f306c2260dd4ebc6f9376882e0abbe23eebbb81
Victor Vinicius <victorsomulex@gmail.com>
2024-07-13 14:35:12 -0300

    fix: varchar -> text

--- src/models/wpp-sessions.entity.ts
+++ src/models/wpp-sessions.entity.ts
@@ -11,7 +11,7 @@ export class WppSessions {
   @Column({ type: 'int', nullable: true })
   customer_id: number | null;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'text'})
   qrcode: string;
 
   @Column({ type: 'varchar', length: 255 })
